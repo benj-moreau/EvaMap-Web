f146df73c8a09511a832e019e34eff92892e4b5a
benjimor <benji_moreau@hotmail.fr>
2020-03-09 16:33:31 +0100

    style for home and form

--- webapp/settings.py
+++ webapp/settings.py
@@ -108,12 +108,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'evamap_web/static/'
+STATIC_ROOT = 'evamap_web/'
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'evamap_web/static'),
+)
 
 try:
     from local_settings import *
