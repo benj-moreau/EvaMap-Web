4e39847aa1abbc36e9ecb35dbfda9bb70ebf78ab
benjimor <benji_moreau@hotmail.fr>
2020-03-11 22:32:07 +0100

    heroku

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from evamap_web.views import home, form, result
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^form/?$', form, name='form'),
     url(r'^result/?$', result, name='result'),
     url(r'', home, name='home')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
