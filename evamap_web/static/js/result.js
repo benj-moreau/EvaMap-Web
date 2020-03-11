688d87be1295ce55ed8a8896a93fe5730d3066c9
benjimor <benji_moreau@hotmail.fr>
2020-03-11 17:06:07 +0100

    result page is working with detailed report

--- evamap_web/static/js/result.js
+++ evamap_web/static/js/result.js
@@ -1,11 +1,36 @@
 var result = new Vue({
     el: '#result',
     data: {
+        total_result: total_score,
+        detailed_results: detailed_result
     },
     methods: {
-        test: function () {
-            console.log(total_score);
-            console.log(detailed_result);
+        get_color: function(score) {
+            score = score * 100;
+            if (score >= 80) {
+                return '#00c7b1';
+            } else if (score >= 40) {
+                return '#efb71b';
+            }
+            return '#ce0037';
+        },
+        get_icon: function(dimension_name) {
+            switch (dimension_name) {
+                case 'Connectability':
+                    return 'fab fa-connectdevelop';
+                case 'Clarity':
+                    return 'fas fa-glasses';
+                case 'Consistency':
+                    return 'fas fa-brain';
+                case 'Availability':
+                    return 'far fa-handshake';
+                case 'Conciseness':
+                    return 'fas fa-compress-arrows-alt';
+                case 'Coverability':
+                    return 'fas fa-table';
+                default:
+                    return 'fas fa-star';
+            }
         }
     }
 });
--- evamap_web/views.py
+++ evamap_web/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from EvaMap.EvaMap import EvaMap
-
+import json
 
 @require_http_methods(['GET'])
 def home(request):
@@ -27,4 +27,4 @@ def result(request):
     # result = evamap.get_complet_result()
     total_score = evamap.get_total_score()
     sorted_result = sorted(result, key=lambda res: (res['score'], res['name']))
-    return render(request, 'result.html', {'total_score': total_score, 'detailed_result': sorted_result})
+    return render(request, 'result.html', {'total_score': total_score, 'detailed_result': json.dumps(sorted_result)})
