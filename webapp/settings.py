27b4ed872382a0391848bbbaa8340c4fb93091c5
benjimor <benji_moreau@hotmail.fr>
2020-03-12 16:08:06 +0100

    settings production ready

--- webapp/settings.py
+++ webapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'qprg!*8!emy+jvaq13&^x&#4dkeekq#fp$ohan@!us0b6@_!j@'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
